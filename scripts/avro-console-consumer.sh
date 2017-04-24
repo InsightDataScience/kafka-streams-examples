e0e8ee620d967d53b643c731d77df70e430a235a
Ronak Nathani <ronaknnathani@gmail.com>
2017-04-24 15:46:58 -0700

    updated avro consumer and added console consumer for word count

--- scripts/avro-console-consumer.sh
+++ scripts/avro-console-consumer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/confluent/bin/kafka-avro-console-consumer --new-consumer --bootstrap-server localhost:9092 --topic $1 --from-beginning
+/usr/local/confluent/bin/kafka-avro-console-consumer --new-consumer --bootstrap-server localhost:9092 --topic $1 --from-beginning
