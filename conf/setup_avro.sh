0f5370c6af4400d3eeacf9b718466e9fc7f79879
Ronak Nathani <ronaknnathani@gmail.com>
2017-04-24 22:58:17 -0700

    removed conf and avro-example dir

--- conf/setup_avro.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget http://www-us.apache.org/dist/avro/stable/py/avro-1.8.1.tar.gz
-tar -zxvf avro-1.8.1.tar.gz
-mv avro-1.8.1 avro
-
-cd avro
-sudo python setup.py install
--- conf/setup_redshift_jdbc_client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://s3.amazonaws.com/redshift-downloads/drivers/RedshiftJDBC42-1.2.1.1001.jar
-mv RedshiftJDBC42-1.2.1.1001.jar /usr/local/confluent/share/java/kafka-connect-jdbc/
