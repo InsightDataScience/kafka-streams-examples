3a22991a7f05545b78ba1b27e217b034644781ce
Ronak Nathani <ronaknnathani@gmail.com>
2017-04-24 23:13:17 -0700

    updated table schema for users table

--- postgres/create_table_postgres.sql
+++ postgres/create_table_postgres.sql
@@ -1,8 +1,8 @@
 CREATE TABLE users
 (
-    id          SERIAL PRIMARY KEY NOT NULL,
+    id          SERIAL NOT NULL,
     name        VARCHAR(100) NOT NULL,
     age         INTEGER,
     updated_at  TIMESTAMP NOT NULL DEFAULT NOW(),
-    PRIMARY KEY(id, name)
+    PRIMARY KEY(id)
 );
